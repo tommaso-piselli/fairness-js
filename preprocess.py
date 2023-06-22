9fa82cb574e370625092e00a70d51badc7fe68e7
tommaso-piselli <43520472+tommaso-piselli@users.noreply.github.com>
2023-06-22 18:35:09 +0200

    Drawing Karate Club, #ToDo: calculate Fairness and SGD, then UI

---
+++ preprocess.py
@@ -0,0 +1,29 @@
+import json
+import networkx as nx
+from sklearn.preprocessing import MinMaxScaler
+import random
+
+# Load the Karate Club graph
+G = nx.karate_club_graph()
+
+# Compute the layout
+pos = nx.spring_layout(G)
+
+# Scale the positions to range between 10 and 490 (to fit a 500x500 SVG element with a little margin)
+scaler = MinMaxScaler((10, 490))
+pos_array = scaler.fit_transform(list(pos.values()))
+
+# Convert the positions back to a dictionary
+scaled_pos = {n: pos_array[i].tolist() for i, n in enumerate(pos.keys())}
+
+# Assign the positions and colors to the nodes in the graph
+for node, data in G.nodes(data=True):
+    data['x'], data['y'] = scaled_pos[node]
+    data['color'] = 'blue' if random.random() < 0.8 else 'red'  # assign color with 80% chance for blue
+
+# Convert the graph to a dictionary
+graph_dict = nx.node_link_data(G)
+
+# Write the graph data to a JSON file
+with open('data/graph.json', 'w') as f:
+    json.dump(graph_dict, f)
--- script.js
+++ script.js
@@ -1,25 +1,49 @@
 d3.json('data/graph.json').then(function(graph) {
-    let svg = d3.select("#graph");
-  
-    // draw nodes
-    let nodes = svg.selectAll("circle")
-      .data(graph.nodes)
-      .enter()
-      .append("circle")
-      .attr("cx", function(d) { return d.x; }) // position based on the x coordinate in the data
-      .attr("cy", function(d) { return d.y; }) // position based on the y coordinate in the data
-      .attr("r", 10)
-      .style("fill", function(d) { return d.color; });
-  
-    // draw edges
-    let edges = svg.selectAll("line")
-      .data(graph.edges)
-      .enter()
-      .append("line")
-      .attr("x1", function(d) { return nodes.data().find(node => node.id === d.source).x; }) // position based on the source node's x coordinate
-      .attr("y1", function(d) { return nodes.data().find(node => node.id === d.source).y; }) // position based on the source node's y coordinate
-      .attr("x2", function(d) { return nodes.data().find(node => node.id === d.target).x; }) // position based on the target node's x coordinate
-      .attr("y2", function(d) { return nodes.data().find(node => node.id === d.target).y; }) // position based on the target node's y coordinate
-      .style("stroke", "black");
-  });
-  
+  let svg = d3.select("#graph");
+
+  // define dimensions
+  let svgWidth = parseFloat(svg.style("width"));
+  let svgHeight = parseFloat(svg.style("height"));
+  console.log(svgWidth, svgHeight)
+  let margin = 10; // leave a small margin around the graph
+
+  // define scales
+  let xExtent = d3.extent(graph.nodes, d => d.x);
+  let yExtent = d3.extent(graph.nodes, d => d.y);
+
+
+  // calculate scales: keep the graph's aspect ratio
+  let xRange = xExtent[1] - xExtent[0];
+  let yRange = yExtent[1] - yExtent[0];
+  let xScale, yScale;
+  if (xRange > yRange) {
+    let scale = (svgWidth - 2 * margin) / xRange;
+    xScale = d3.scaleLinear().domain(xExtent).range([margin, svgWidth - margin]);
+    yScale = d3.scaleLinear().domain(yExtent).range([(svgHeight - yRange * scale) / 2, (svgHeight + yRange * scale) / 2]);
+  } else {
+    let scale = (svgHeight - 2 * margin) / yRange;
+    yScale = d3.scaleLinear().domain(yExtent).range([margin, svgHeight - margin]);
+    xScale = d3.scaleLinear().domain(xExtent).range([(svgWidth - xRange * scale) / 2, (svgWidth + xRange * scale) / 2]);
+  }
+
+  // draw edges
+  let links = svg.selectAll("line")
+    .data(graph.links)
+    .enter()
+    .append("line")
+    .attr("x1", function(d) { return xScale(graph.nodes[d.source].x); }) // position based on the source node's x coordinate
+    .attr("y1", function(d) { return yScale(graph.nodes[d.source].y); }) // position based on the source node's y coordinate
+    .attr("x2", function(d) { return xScale(graph.nodes[d.target].x); }) // position based on the target node's x coordinate
+    .attr("y2", function(d) { return yScale(graph.nodes[d.target].y); }) // position based on the target node's y coordinate
+    .style("stroke", "black");
+
+  // draw nodes
+  let nodes = svg.selectAll("circle")
+    .data(graph.nodes)
+    .enter()
+    .append("circle")
+    .attr("cx", function(d) { return xScale(d.x); }) // position based on the x coordinate in the data
+    .attr("cy", function(d) { return yScale(d.y); }) // position based on the y coordinate in the data
+    .attr("r", 5)
+    .style("fill", function(d) { return d.color; }); // color based on data
+});
